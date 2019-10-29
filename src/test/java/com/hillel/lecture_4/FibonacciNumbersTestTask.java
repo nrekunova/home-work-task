c4c4e6f126c2b91608c32dd9e6a855dd2a34d7dd
Alexey Panashchenko <alpa@ciklum.com>
2019-10-29 13:33:01 +0200

    Fix FibonacciNumbersTestTask

--- src/test/java/com/hillel/lecture_4/FibonacciNumbersTestTask.java
+++ src/test/java/com/hillel/lecture_4/FibonacciNumbersTestTask.java
@@ -13,7 +13,7 @@ public class FibonacciNumbersTestTask {
 
     @Test
     public void fromDecimalToHexadecimalTest() {
-        int[] expectedResult = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987 };
+        int[] expectedResult = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181 };
 
         assertEquals(fibonacciNumbersChecker.getFibonacciNumbers(20), expectedResult) ;
     }

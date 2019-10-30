be63648a40ab30a42d7092cac4da958caef354c2
nataliia <n_rekunova@hotmail.com>
2019-10-30 22:05:13 +0200

    2 last tests

--- src/main/java/com/hillel/lecture_4/BinaryToDecimalChecker.java
+++ src/main/java/com/hillel/lecture_4/BinaryToDecimalChecker.java
@@ -10,21 +10,32 @@ public class BinaryToDecimalChecker {
     @Step
     public int fromDecimalToBinary(int value) {
 
-//        TODO implements result
         int result = 0;
+        int i = 0;
+        while (value > 0) {
+            int rest = value % 2;
+            if (rest > 0) {
+               value = value - rest;
+               result = result + ((int) Math.pow(10, i));
+            }
+            value = value / 2;
+            i++;
+        }
+
 
         return result;
     }
 
     @Step
     public int fromBinaryToDecimal(int value) {
-
-        // 101
-        // 4+1
-        // 111 = 4 +2 +1 =7
-        // 1000 = 8
+        String vals = String.valueOf(value);
         int result = 0;
-
+        for (int i = 0; i < vals.length(); i++) {
+            char c = vals.charAt(vals.length() - (i +1));
+            if (c == '1') {
+                result = result + ((int) Math.pow(2, i));
+            }
+        }
         return result;
     }
 }
--- src/main/java/com/hillel/lecture_4/DecimalToHexadecimalChecker.java
+++ src/main/java/com/hillel/lecture_4/DecimalToHexadecimalChecker.java
@@ -9,9 +9,32 @@ public class DecimalToHexadecimalChecker {
 
     @Step
     public String fromDecimalToHexadecimal(int value) {
-
-//        TODO implements result
         String result = "";
+        int i = 0;
+        while (value > 0) {
+            int rest = value % 16;
+            value = value - rest;
+            String c = "";
+            if (rest == 10) {
+                c = "A";
+            } else if (rest == 11) {
+                c = "B";
+            } else if (rest == 12) {
+                c = "C";
+            } else if (rest == 13) {
+                c = "D";
+            } else if (rest == 14) {
+                c = "E";
+            } else if (rest == 15) {
+                c = "F";
+            } else {
+                c = String.valueOf(rest);
+            }
+            result = c + result;
+            value = value / 16;
+            i++;
+        }
+
 
         return result;
     }
@@ -19,9 +42,27 @@ public class DecimalToHexadecimalChecker {
     @Step
     public int fromHexadecimalToDecimal(String value) {
 
-//        TODO implements result
         int result = 0;
-
+        for (int i = 0; i < value.length(); i++) {
+            char c = value.charAt(value.length() - (i + 1));
+            int num = 0;
+            if (c == 'A') {
+                num = 10;
+            } else if (c == 'B') {
+                num = 11;
+            } else if (c == 'C') {
+                num = 12;
+            } else if (c == 'D') {
+                num = 13;
+            } else if (c == 'E') {
+                num = 14;
+            } else if (c == 'F') {
+                num = 15;
+            } else {
+                num = Integer.valueOf(String.valueOf(c));
+            }
+            result = result + num * ((int) Math.pow(16, i));
+        }
         return result;
     }
 }

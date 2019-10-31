ddcd9753d966073551cf56ab45d1f4fc03a80faa
nataliia <n_rekunova@hotmail.com>
2019-10-31 13:01:28 +0200

    commit message

--- src/main/java/com/hillel/lecture_2/Formulas.java
+++ src/main/java/com/hillel/lecture_2/Formulas.java
@@ -9,35 +9,31 @@ public class Formulas {
 
     @Step
     public double calculate9Formula(double x) {
-        double result = 1.1 * Math.pow(Math.E,-x) + Math.abs(Math.cos(Math.sqrt(Math.PI * x))) - 3.0 / 8.0;
+        double result = 1.1 * Math.pow(Math.E, -x) + Math.abs(Math.cos(Math.sqrt(Math.PI * x))) - 3.0 / 8.0;
         return result;
     }
 
     @Step
     public double calculate10Formula(double x) {
-        double result = 0.0;
-//        TODO implement formula 10
+        double result = (1.0 / 3.0) * Math.sqrt(Math.abs(Math.sin(x))) * Math.pow(Math.pow(Math.E, 0.12 * x), (1.0 / 3.0));
         return result;
     }
 
     @Step
     public double calculate11Formula(double x) {
-        double result = 0.0;
-//        TODO implement formula 11
+        double result = 2 * Math.PI * x - (Math.abs(Math.sin(Math.sqrt(10.5 * x))) / (Math.pow(x * x, 1.0 / 3.0) + 1.0 / 7.0));
         return result;
     }
 
     @Step
     public double calculate12Formula(double x) {
-        double result = 0.0;
-//        TODO implement formula 12
+        double result = Math.log(Math.sqrt(Math.abs(2.0 - x)) + 1.2) / (2.0 + Math.pow(Math.E, -x)) + Math.pow(2.0 / x, 1.0 / 3.0);
         return result;
     }
 
     @Step
     public double calculate13Formula(double x) {
-        double result = 0.0;
-//        TODO implement formula 13
+        double result = Math.pow(Math.exp(-2.0 + x), 1.0 / 5.0) / (Math.sqrt(x * x + Math.pow(x, 4.0) + Math.log(Math.abs(x - 3.14))));
         return result;
     }
 }

aeae436a3a4fcd6742415d085ca1e22ad3448466
Alexey Panashchenko <alpa@ciklum.com>
2019-11-08 10:42:55 +0200

    Add task for lecture 7

---
+++ src/test/java/com/hillel/lecture_7/FractionNumber.java
@@ -0,0 +1,9 @@
+package com.hillel.lecture_7;
+
+/**
+ * Created by alpa on 11/8/19
+ */
+public class FractionNumber {
+
+
+}
---
+++ src/test/java/com/hillel/lecture_7/FractionNumbersTestTask.java
@@ -0,0 +1,18 @@
+package com.hillel.lecture_7;
+
+/**
+ * Write class to represent FractionNumbers
+ * This class should contain two fields.
+ * This class should contain methods to perform basic math operations:
+ * plus, minus, multiply, divide
+ * Also, this class should have toString method to print numbers into standard output
+ * Add tests for check functionality of FractionNumber class
+ */
+public class FractionNumbersTestTask {
+
+
+
+
+
+
+}

7220ce983e917acf85335a2cd4050688406dcc93
nataliia <n_rekunova@hotmail.com>
2019-11-08 21:12:36 +0200

    last task of Lecture6

--- src/main/java/com/hillel/lecture_6/ProductCodeValidationChecker.java
+++ src/main/java/com/hillel/lecture_6/ProductCodeValidationChecker.java
@@ -11,9 +11,19 @@ public class ProductCodeValidationChecker {
     @Step
     public boolean validateCode(String code) {
 
-//        TODO implements result
-        boolean isValid = false;
+        String[] parts = code.split(" ");
+        String nums = "";
+        for (char letter: parts[0].toCharArray()) {
+            if (letter >= '0' && letter <= '9') {
+                nums = nums + letter;
+            }
+        }
 
-        return isValid;
+        int n1 = Integer.valueOf(nums.substring(0, 2));
+        int n2 = Integer.valueOf(nums.substring(2, 4));
+        int n3 = Integer.valueOf(nums.substring(4));
+        int total = Integer.valueOf(parts[1]);
+
+        return n1*n2*n3 == total;
     }
 }
